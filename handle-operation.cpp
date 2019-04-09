7060c7c750bb02a79f1eac034fb317afc8fa8ab2
Luiz <37978726+luizmariz@users.noreply.github.com>
2019-04-09 18:37:55 -0300

    *deleted out files

--- handle-operation.cpp
+++ handle-operation.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstdlib>
 #include <stdexcept>
 
 template <typename T>
@@ -48,9 +49,7 @@ class _Stack {
     }
 
     void print() {
-      std::printf("[ ");
       for (size_t i = 0; i < this->length; ++i) std::printf("%d ", int(this->data[i]));
-      std::printf("]\n");
     }
 
     ~_Stack() {
@@ -59,19 +58,89 @@ class _Stack {
 };
 
 template <typename T>
-struct List {
-  T* content;
-  size_t size;
+class _Queue {
+  private:
+
+    T* data;
+    size_t capacity;
+    size_t length;
+    size_t out;
+
+  public:
+
+    _Queue(): data(NULL), capacity(0), length(0), out(0) {}
+
+    size_t len() { return this->length; }
+
+    void push(const T element) {
+      if (this->data == NULL) {
+        this->data = new T[1];
+        ++this->capacity;
+
+      } else if (this->length == this->capacity) {
+        T* newData = new T[sizeof(this->capacity*2)];
+
+        for (size_t i = 0; i != this->capacity; ++i) {
+          newData[i] = this->data[i];
+        }
+
+        delete[] this->data;
+        this->data = newData;
+        this->capacity *= 2;
+
+      }
+
+      this->data[this->length++] = element;
+
+    }
+
+    T pop() {
+      if (this->out < this->length) return this->data[this->out++];
+      else throw std::underflow_error("Queue overflow exception");
+      --this->length;
+    }
+
+    T value() {
+      if (this->out < this->length) return this->data[this->out];
+      else throw std::runtime_error("Can't get value of an empty Queue");
+    }
+
+    void print() {
+      for (size_t i = this->out; i < this->length; ++i) std::printf("%c ", (char)this->data[i]);
+    }
+
+    ~_Queue() {
+      delete[] this->data;
+    }
 };
+
+void expToPolishNotation(char *buff, _Stack<char> &s, _Queue<char> &q);
+void solveAndPrintPoslih( _Queue<char> &q);
 
 int main() {
 
-  _Stack<float> s;
-  char* buff = new char[100];
+  _Stack<char> s;
+  _Queue<char> q;
 
-  while (std::fgets(buff, 100, stdin)) {
+  char* buff = new char[sizeof(char)*50];
 
-    if (std::isdigit(buff[0])) s.push(std::atof(buff));
+  while (std::fgets(buff, 50, stdin)) {
+
+    expToPolishNotation(buff, s, q);
+    solveAndPrintPoslih(q);
+
+  }
+
+  delete[] buff;
+  return 0;
+}
+
+void solveAndPrintPoslih( _Queue<char> &q) {
+
+  _Stack<int> s;
+
+  for ( size_t i = 0; i < q.len(); ++i ) {
+    if (std::isdigit(q.value())) s.push((int)(q.value() - '0'));
     else {
 
       float res, x1, x2;
@@ -79,7 +148,7 @@ int main() {
       x2 = s.pop();
       x1 = s.pop();
 
-      switch (buff[0]) {
+      switch (q.value()) {
         case '+': res = x1 + x2; break;
         case '-': res = x1 - x2; break;
         case '*': res = x1 * x2; break;
@@ -89,10 +158,56 @@ int main() {
 
       s.push(res);
     }
+    q.pop();
   }
 
   s.print();
+}
 
-  delete[] buff;
-  return 0;
+void expToPolishNotation(char *buff, _Stack<char> &s, _Queue<char> &q) {
+  for ( size_t i = 0; i < 50; ++i ) {
+
+    if ( buff[i] == '\0') {
+      break;
+    }
+
+    if (std::isdigit(buff[i])) q.push(buff[i]);
+    else {
+
+      if (s.len() == 0) s.push(buff[i]);
+      else {
+
+        switch (buff[i]){
+          case '(': break;
+
+          case ')':
+            while (s.value() != '(') {
+              q.push(s.pop());
+            }
+            s.pop();
+            break;
+
+          case '*':
+            while (s.value() == '*' || s.value() == '/') {
+              q.push(s.pop());
+            }
+            break;
+
+          case '/':
+            while (s.value() == '*' || s.value() == '/') {
+              q.push(s.pop());
+            }
+            break;
+
+          default:
+            while (s.value() == '*' || s.value() == '/' || s.value() == '+' || s.value() == '-') {
+              q.push(s.pop());
+            }
+            break;
+        }
+
+        if (buff[i] != ')') s.push(buff[i]);
+      }
+    }
+  }
 }
