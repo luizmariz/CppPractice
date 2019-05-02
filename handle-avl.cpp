c40634cb131860e1cbf73a9f2bd43cae87702c4c
Luiz <37978726+luizmariz@users.noreply.github.com>
2019-05-02 00:56:12 -0300

    added: AVL tree implementation

---
+++ handle-avl.cpp
@@ -0,0 +1,166 @@
+#include <cstdio>
+#include <cstdlib>
+
+struct Node {
+  struct Node* esq;
+  struct Node* dir;
+  int key;
+};
+
+class _AVLTree {
+  private:
+
+    Node* root;
+
+    Node* createNode( int key ) {
+      Node* pt = new Node;
+
+      pt->esq = NULL;
+      pt->dir = NULL;
+      pt->key = key;
+
+      return pt;
+    }
+
+    void addNodeToTree( Node *pt, int key) {
+
+      if ( key < pt->key ) {
+        if ( pt->esq == NULL ) pt->esq = this->createNode(key);
+        else this->addNodeToTree(pt->esq, key);
+      }
+
+      if ( key >= pt->key ) {
+        if ( pt->dir == NULL ) pt->dir = this->createNode(key);
+        else this->addNodeToTree(pt->dir, key);
+      }
+
+    }
+
+    void printPreorder( Node *pt ) {
+      if ( pt != NULL ) {
+        std::printf("%d-- e:%d d:%d\n", pt->key, pt->esq? pt->esq->key:100, pt->dir ? pt->dir->key:100);
+        this->printPreorder( pt->esq );
+        this->printPreorder( pt->dir );
+      }
+    }
+
+    void freeTree( Node *&pt ) {
+      if ( pt != NULL ) {
+        this->freeTree(pt->esq);
+        this->freeTree(pt->dir);
+
+        delete pt;
+      }
+    }
+
+    int getMaxLevel( Node *pt ) {
+      if ( pt != NULL && pt->esq == NULL && pt->dir == NULL ) {
+        return 0;
+      }
+
+      if ( pt == NULL ) {
+        return -1;
+      }
+
+      int levelEsq = 1+getMaxLevel(pt->esq), levelDir = 1+getMaxLevel(pt->dir);
+      return levelEsq >= levelDir ? levelEsq : levelDir;
+    }
+
+    int getBalanceK( Node *pt ) {
+      return pt == NULL ? -1 : this->getMaxLevel(pt->esq) - this->getMaxLevel(pt->dir);
+    }
+
+    Node* rotateLeft( Node *pt ) {
+      Node* newParent;
+
+      newParent = pt->dir;
+      pt->dir = newParent->esq != NULL ? newParent->esq : NULL;
+      newParent->esq = pt;
+
+      return newParent;
+    }
+
+    Node* rotateRight( Node *pt ) {
+      Node* newParent;
+
+      newParent = pt->esq;
+      pt->esq = newParent->dir != NULL ? newParent->dir : NULL;
+      newParent->dir = pt;
+
+      return newParent;
+    }
+
+    Node* balance( Node *pt ) {
+      Node* newRoot = NULL;
+
+      if ( pt != NULL ) {
+        pt->esq = this->balance(pt->esq);
+        pt->dir = this->balance(pt->dir);
+      }
+
+      int bk = this->getBalanceK(pt);
+
+      if ( bk <= 1 && bk >= -1 ) newRoot = pt;
+
+      if ( bk <= -2 ) {
+        if ( this->getBalanceK(pt->dir) < 1 ) newRoot = this->rotateLeft(pt);
+        else {
+          pt->dir = this->rotateRight(pt->dir);
+          newRoot = this->rotateLeft(pt);
+        }
+      }
+
+      if ( bk >= 2 ) {
+        if ( this->getBalanceK(pt->esq) < 1 ) newRoot = this->rotateRight(pt);
+        else {
+          pt->esq = this->rotateLeft(pt->esq);
+          newRoot = this->rotateRight(pt);
+        }
+      }
+
+      return newRoot;
+    }
+
+  public:
+
+    _AVLTree(): root( NULL ) {}
+
+    void push(int key) {
+      if ( this->root == NULL ) this->root = createNode(key);
+      else this->addNodeToTree(this->root, key);
+
+      this->root = this->balance(this->root);
+    }
+
+    void print() {
+      this->printPreorder(this->root);
+    }
+
+    int height() {
+      return this->getMaxLevel(this->root);
+    }
+
+    ~_AVLTree() {
+      this->freeTree(this->root);
+    }
+};
+
+int main() {
+  _AVLTree tree;
+
+  tree.push(1);
+  tree.push(0);
+  tree.push(2);
+  tree.push(2);
+  tree.push(3);
+  tree.push(2);
+  tree.push(2);
+  tree.push(5);
+  tree.push(17);
+  tree.push(2);
+  tree.print();
+
+  std::printf("\n%d\n", tree.height());
+
+  return 0;
+}
